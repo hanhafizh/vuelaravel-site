65a107ddeac74fef2afd2c6c118946e8ac31cb3a
hanhafizh <farhanhafizh770@gmail.com>
2024-06-22 13:35:44 +0700

    header-components

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,18 +1,20 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <!-- Header Section Begin -->
+    <HeaderShayna />
+    <!-- Header End -->
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+// import HelloWorld from "@/components/HelloWorld.vue";
+import HeaderShayna from "@/components/HeaderShayna.vue";
 
 export default {
-  name: 'HomeView',
+  name: "HomeView",
   components: {
-    HelloWorld
-  }
-}
+    HeaderShayna,
+  },
+};
 </script>
